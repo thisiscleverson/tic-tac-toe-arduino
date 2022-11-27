5d1056ba3771ad20e7ba14342da6ff1371221fc4
cleverson <cleverson2020333@gmail.com>
2022-11-26 23:41:11 -0300

    first commit

---
+++ tic-tac-toe-arduino-demo1.ino
@@ -0,0 +1,155 @@
+/*
+ name: tic-tac-toe with arduino (jogo da velha com arduino)
+ type: demo
+ version: 01
+ repository: 
+*/
+
+//variáveis
+
+byte pins_buttons[9] = {5,6,7,8,9,10,11,12,13}; // Lista de pinos digitais para os botões
+
+
+int board[9] = { // tabuleiro vazio
+                -1,-1,-1,
+                -1,-1,-1,
+                -1,-1,-1
+                }; 
+
+
+int winningSequences[8][3] = {
+                              {0,1,2},
+                              {3,4,5},
+                              {6,7,8},
+                              {0,3,6},
+                              {1,4,6},
+                              {2,5,8},
+                              {0,4,8},
+                              {2,4,6}
+                            };
+
+bool gameover = false; 
+bool debug    = true; // variável para mostrar todos os logs no serial monitor
+bool symbols  = 1;    // 1 para [X] & 0 para [O]
+
+
+//Funções
+bool change(){
+  symbols = !symbols;
+  return symbols;
+}
+
+
+int getButtonAction(){ // capturar qual botão foi precionado
+  for(int x=0; x<9; x++){
+    if(digitalRead(pins_buttons[x]) != 1){
+      return x; // retornar o index do botão precionado
+    };
+  };
+  return -1; // retornar -1 se nenhum botão foi apertado
+}
+
+
+void make_play(int position){ // inserir o [X] ou [O] no tabuleiro
+  bool symbols;
+  int winningSequencesIndex;
+  if(board[position] == -1){
+    symbols = change();
+    board[position] = symbols;
+  };
+
+  winningSequencesIndex = checkWinningSequences(symbols); // verificar a sequência vencedora 
+
+  if(winningSequencesIndex == -1){
+    checkTie(); // verificar se deu um empate
+  }
+}
+
+
+int checkWinningSequences(bool symbols){
+  for(int i=0; i<8; i++){
+    if(board[winningSequences[i][0]] == symbols && board[winningSequences[i][1]] == symbols && board[winningSequences[i][2]] == symbols){
+      gameover = true;
+      return i; 
+    };
+  };
+  return -1;
+}
+
+
+void checkTie(){ // verificar empate
+  for(int i=0; i<9; i++){
+    if(board[i] == -1){
+      return;
+    }
+  };
+
+  gameover = true;
+}
+
+
+void reset(){
+  // limpar o tabuleiro  
+  for(int i=0; i<9; i++){
+    board[i] = -1;
+  };
+  symbols  = 1;
+  gameover = false;  
+}
+
+
+void showBoard(){ // mostrar o tabuleiro no monitor serial
+  for(int i=0; i<9;){
+      for(int r=0; r<3; r++){
+        Serial.print(String(board[i]) + " ");
+        i++;      
+      };
+      Serial.println();
+    };
+}
+
+//setup
+void setup() {
+
+  Serial.begin(9600);
+
+  // ativar os botões como entrada
+  for(int i=0; i<9; i++){
+    pinMode(pins_buttons[i], INPUT_PULLUP);
+  };
+
+  // mostrar o tabuleiro no monitor serial
+  if(debug){
+    Serial.println("Board:");
+    showBoard(); 
+  }
+
+}
+
+void loop() {
+  
+  int buttonIndex = getButtonAction();
+  
+  if(buttonIndex != -1 && !gameover){
+      make_play(buttonIndex);
+      //mostrar tabuleiro
+      
+      if(debug){
+        Serial.println("Board:");
+        showBoard();
+      }
+  };
+
+  // resetar o jogo
+  if(gameover){
+    delay(2000);
+    reset();
+
+    if(debug){
+      Serial.println("Gamer Over");
+      showBoard();
+    }
+  };
+  
+  delay(100);
+}
